95ea02740be83b2bb48dbf4ad80257cf4f7842e5
Laurens van der Maas <laurens@Laurenss-MacBook-Air.local>
2018-12-01 12:44:05 +0100

    proper steps

--- index.js
+++ index.js
@@ -80,11 +80,11 @@ class Main {
             // "8718858075485",
             // "8718053679570",
             // "8718053671819",
-            "8718053671901",
-            "8718053671918",
-            "8719743060395",
+            // "8718053671901",
+            // "8718053671918",
+            // "8719743060395",
             "8719743060487",
-            "8718053677972"
+            // "8718053677972"
         ];
 
         this.app = express();
@@ -93,23 +93,23 @@ class Main {
         this.app.get('*.*', express.static(process.cwd() + '/prices', {maxAge: 160000}));
 
 
-
         this.app.use("/", (req, res) => {
             res.send("works");
         });
 
-        fs.readFile(process.cwd() + '/ean_check.csv', 'utf8', (err, data) => {
+        fs.readFile(process.cwd() + '/ean_check.csv', 'utf8', async (err, data) => {
             if (err) throw err;
-            if(data) {
+            if (data) {
 
-                this.skus = data.split("\n").slice(1).map((str) => "0" + str.split(";")[1]).filter(sku => sku.length > 1);
+                // this.skus = data.split("\n").slice(1).map((str) => "0" + str.split(";")[1]).filter(sku => sku.length > 1).slice(0, 1);
+
                 console.log(this.skus);
-                (async () => {
-                    this.browser = await puppeteer.launch();
 
-                    // await this.save();
+                this.browser = await puppeteer.launch({pipe: true}).catch(e => {
+                    console.log(e);
+                });
 
-                })();
+                this.save();
             }
 
         });
@@ -118,7 +118,9 @@ class Main {
     }
 
     save() {
+
         this.endCount = this.skus.length;
+        console.log("Start doing", this.endCount, "sku's");
         this.count = 0;
         // this.saveJson(this.skus[0]);
         this.skus.forEach(this.saveJson.bind(this))
@@ -135,52 +137,67 @@ class Main {
     async saveJson(sku) {
         let error = false;
 
-        console.log("started sku", this.count);
-
         const page = await this.browser.newPage();
         await page.goto("http://google.nl");
-
-        await page.waitFor(2000);
-
         await page.type('input[name="q"]', sku);
+
+        console.log("Started step 1");
 
         await page.keyboard.down('Enter').catch(() => {
             error = true;
-            console.log(`error step 1, ${sku}`);
             page.screenshot({path: process.cwd() + `/errors/error-${sku}.png`, fullPage: true});
         });
 
+
         if (!error) {
-            await page.waitFor(2000);
+            console.log("Started step 2");
 
-            await page.click('#top_nav .hdtb-mitem:last-child a');
-            await page.waitFor(2000);
+            await page.waitForSelector("#top_nav");
 
-            let resp = await page.click('#rcnt a.vjtvke').catch(e => {
+            await page.evaluate(() => {
+                return [].slice.call(document.querySelectorAll("#top_nav a")).find(a => a.innerText.includes("Shopping")).click()
+            }).catch((e) => {
+                error = true;
+                page.screenshot({path: process.cwd() + `/errors/error-${sku}.png`, fullPage: true});
+            });
+
+
+            console.log("Started step 3");
+
+            await page.waitForSelector("#rcnt");
+            await page.click('#rcnt a.vjtvke').catch(e => {
                 error = true;
             });
 
             if (!error) {
-                await page.waitFor(2000);
-
+                await page.waitForSelector("#search");
                 await page.click("#search a").catch(() => {
                     error = true;
 
-                    console.log(`error step 2, ${sku}`);
+                    console.log(`error step 3, ${sku}`);
                     page.screenshot({path: process.cwd() + `/errors/error-${sku}.png`, fullPage: true});
                 });
 
 
                 if (!error) {
-                    await page.waitFor(2000);
-                    await page.evaluate(() => {
-                        [].slice.call(document.querySelectorAll("a")).find(a => a.innerText.includes("Prijzen vergelijken")).click()
-                    }).catch(() => {
-                        error = true;
+                    await page.waitFor("#rcnt");
 
-                        console.log(`error step 3, ${sku}`);
+                    await page.evaluate(() => {
+                        return [].slice.call(document.querySelectorAll("a")).find(a => a.innerText.includes("Nieuwe artikelen")).click()
+                    }).catch((e) => {
+                        error = true;
+                        console.log(`error step 4, ${sku}`);
                         page.screenshot({path: process.cwd() + `/errors/error-${sku}.png`, fullPage: true});
                     });
+
+                    // await page.evaluate(() => {
+                    //     [].slice.call(document.querySelectorAll("a")).find(a => a.innerText.includes("Prijzen vergelijken")).click()
+                    // }).catch(() => {
+                    //     error = true;
+                    //
+                    //     console.log(`error step 4, ${sku}`);
+                    //     page.screenshot({path: process.cwd() + `/errors/error-${sku}.png`, fullPage: true});
+                    // });
 
 
                     if (!error) {
